e02e5043ef1a9100dcd70456782fadface1993e5
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2019-09-12 11:07:52 +1000

    Add .swift-version for SwiftFormat, run SwiftFormat on Sources/

--- Sources/Margherita/AppDelegate.swift
+++ Sources/Margherita/AppDelegate.swift
@@ -10,10 +10,9 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let window = UIWindow()
         window.rootViewController = ViewController()
@@ -21,6 +20,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window = window
         return true
     }
-
 }
-
