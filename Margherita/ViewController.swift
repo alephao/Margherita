16d6aed4b63bd14ef4a28b7cf9a8ecb1992eb74a
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2019-09-12 10:03:24 +1000

    Init project

---
+++ Margherita/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Margherita
+//
+//  Created by Aleph Retamal on 12/9/19.
+//  Copyright © 2019 alephao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
